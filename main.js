14f2c58e30e83d8d7982ca05cfeaf72168a21808
Richard Turek <rjt1357@gmail.com>
2019-11-30 16:30:30 -0500

    limit key row width; sp between fretboards

--- main.js
+++ main.js
@@ -173,7 +173,7 @@ function allocStrings(fbNum)
       if (j == 0)  {
         spot.fretType = "nut"
       }
-      else if (j==3 || j==5 || j==7 || j==12 )   {
+      else if (j==3 || j==5 || j==7 || j==9 || j==12 )   {
         spot.fretType = "marker"
       }
       else {
@@ -326,7 +326,7 @@ function tdLabels(fbStrings, row, col)
       } else if (showNoteNames)   {
         innerStuff(fbStrings[i][j].td, fbStrings[i][j].noteName);
       } else {
-        innerStuff(fbStrings[i][j].td, "");
+        innerStuff(fbStrings[i][j].td, "-");
       }
     }
   }
